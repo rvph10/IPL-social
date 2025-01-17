1a97f551644c390eb079e884bf9d512e44770449
rvph10 <rgenu10@gmail.com>
2025-01-17 16:23:43 +0100

    Add additional tests for password validation: require at least one number and disallow "IPL" in any case

--- test/passwordChecker.test.js
+++ test/passwordChecker.test.js
@@ -11,4 +11,20 @@ describe('Password Validator', () => {
         expect(isValidPassword('Abcd1234')).toBe(false);
         expect(isValidPassword('Abcd@123')).toBe(true);
     });
+
+    test('should require at least one number', () => {
+        expect(isValidPassword('Abcd@efg')).toBe(false);
+        expect(isValidPassword('Abcd@123')).toBe(true);
+    });
+
+    test('should not allow "IPL" in any case', () => {
+        expect(isValidPassword('iPl@1234')).toBe(false);
+        expect(isValidPassword('IPL@1234')).toBe(false);
+        expect(isValidPassword('ipl@1234')).toBe(false);
+        expect(isValidPassword('Abcd@123')).toBe(true);
+    });
+
+    test('should validate a correct password', () => {
+        expect(isValidPassword('StrongP@ss123')).toBe(true);
+    });
 });
